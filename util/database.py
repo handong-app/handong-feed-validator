d194062a948c3ec2da29899fec5a43ee104084b4
callendev20 <callendev20@gmail.com>
2024-09-03 11:45:21 +0900

    :pen: Fix typo - database.py

--- util/database.py
+++ util/database.py
@@ -2,13 +2,14 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from urllib.parse import quote
 
 load_dotenv()
 
 db_host = os.getenv('DB_HOST')
 db_name = os.getenv('DB_NAME')
 db_username = os.getenv('DB_USERNAME')
-db_password = os.getenv('DB_PASSWORD')
+db_password = quote(os.getenv('DB_PASSWORD'))
 db_port = os.getenv('DB_PORT')
 
 DATABASE_URL = f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
