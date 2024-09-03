ae68d7ef7e3081a325e79fd71fa53ed5bfefda68
callendev20 <callendev20@gmail.com>
2024-09-04 00:06:26 +0900

    :white_check_mark: Add db_insert function

--- util/database.py
+++ util/database.py
@@ -1,4 +1,5 @@
 import os
+from sqlalchemy import text
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
@@ -16,3 +17,27 @@ DATABASE_URL = f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def db_insert(session, message_id, chat_id, client_message_id, room_id, sent_at, user_id, message, current_time, duplicate_count, original_id):
+    session.execute(text(
+        """
+        INSERT INTO mydb_TbKaFeed 
+        (id, chatId, clientMessageId, roomId, sentAt, userId, message, createdDate, modifiedDate, duplicate_count, original_message_id, deleted)
+        VALUES (:id, :chatId, :clientMessageId, :roomId, :sentAt, :userId, :message, :createdDate, :modifiedDate, :duplicate_count, :original_message_id, :deleted)
+        """
+    ), {
+        "id": message_id,
+        "chatId": chat_id,
+        "clientMessageId": client_message_id,
+        "roomId": room_id,
+        "sentAt": sent_at,
+        "userId": user_id,
+        "message": message,
+        "createdDate": current_time,
+        "modifiedDate": current_time,
+        "duplicate_count": duplicate_count,
+        "original_message_id": original_id,
+        "deleted": "N"
+    })
+    session.commit()
