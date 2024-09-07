69e15f9fb1bccb51b43022f6dc750426b2ca2ce0
callendev20 <callendev20@gmail.com>
2024-09-08 00:28:58 +0900

    🗑️ Remove qoute that deprecated

--- util/database.py
+++ util/database.py
@@ -4,16 +4,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
-from urllib.parse import quote
 
 load_dotenv()
 
 db_host = os.getenv('DB_HOST')
 db_name = os.getenv('DB_NAME')
 db_username = os.getenv('DB_USERNAME')
-db_password = quote(os.getenv('DB_PASSWORD'))
+db_password = os.getenv('DB_PASSWORD')
 db_port = os.getenv('DB_PORT')
-
 DATABASE_URL = f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 engine = create_engine(DATABASE_URL)
