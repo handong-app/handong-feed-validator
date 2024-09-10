b377cebf10bc9cfca09fd4adf33002c02d33f33f
callendev20 <callendev20@gmail.com>
2024-09-11 00:51:29 +0900

    :wastebasket: Deprecate db_insert to database.py - move to service section

--- util/database.py
+++ util/database.py
@@ -1,5 +1,4 @@
 import os
-from sqlalchemy import text
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -25,34 +24,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-def db_insert(session, message_id, chat_id, client_message_id, room_id, sent_at, user_id, message, current_time, duplicate_count, original_id):
-    try:
-        session.execute(text(
-            """
-            INSERT INTO mydb_TbKaFeed 
-            (id, chatId, clientMessageId, roomId, sentAt, userId, message, createdDate, modifiedDate, duplicate_count, original_message_id, deleted)
-            VALUES (:id, :chatId, :clientMessageId, :roomId, :sentAt, :userId, :message, :createdDate, :modifiedDate, :duplicate_count, :original_message_id, :deleted)
-            """
-        ), {
-            "id": message_id,
-            "chatId": chat_id,
-            "clientMessageId": client_message_id,
-            "roomId": room_id,
-            "sentAt": sent_at,
-            "userId": user_id,
-            "message": message,
-            "createdDate": current_time,
-            "modifiedDate": current_time,
-            "duplicate_count": duplicate_count,
-            "original_message_id": original_id,
-            "deleted": "N"
-        })
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        print(f"데이터 삽입 중 문제 발생: {e}")
-    finally:
-        print("데이터가 성공적으로 삽입되었습니다.")
-        session.close()
