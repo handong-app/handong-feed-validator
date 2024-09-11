9689c9a036d912859287b59a04913f6250135136
callendev20 <callendev20@gmail.com>
2024-09-12 01:45:29 +0900

    :white_check_mark: Add update_last_sent_at - TbSubjectService

--- services/tb_subject_service.py
+++ services/tb_subject_service.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+from sqlalchemy import text
 from sqlalchemy.orm import Session
 
 from models import TbSubject
+from schemas.validate_dto import ValidateDto
 
 
 class TbSubjectService:
@@ -19,3 +20,35 @@ class TbSubjectService:
             session.rollback()
             print(f"Subject 생성 중 오류 발생: {e}")
             raise
+
+    @staticmethod
+    def update_last_sent_at(session: Session, dto: ValidateDto.ValidateReqDto, subject_id: int):
+        session.execute(text(
+            """
+            UPDATE TbSubject
+            SET last_sent_at = :last_sent_at, last_sent_chat_id = :last_sent_chat_id
+            WHERE id = :subject_id
+            """
+        ), {
+            "last_sent_at": dto.sent_at,
+            "last_sent_chat_id": dto.chat_id,
+            "subject_id": subject_id
+        })
+
+        session.commit()
+
+    @staticmethod
+    def update_last_sent_chat(session: Session, chat_id: int, subject_id: int, sent_at: int):
+        session.execute(text(
+            """
+            UPDATE TbSubject
+            SET last_sent_at = :last_sent_at, last_sent_chat_id = :last_sent_chat_id
+            WHERE id = :subject_id
+            """
+        ), {
+            "last_sent_at": sent_at,
+            "last_sent_chat_id": chat_id,
+            "subject_id": subject_id
+        })
+
+        session.commit()
