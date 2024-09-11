ec7ad62e961452ea30d360ebebe856a41150a058
callendev20 <callendev20@gmail.com>
2024-09-12 01:44:44 +0900

    :white_check_mark: Add serv dtos

--- schemas/validate_dto.py
+++ schemas/validate_dto.py
@@ -13,3 +13,20 @@ class ValidateDto:
         message_id: str
         is_duplicate: bool
         subject_id: int
+
+    class GetDistanceServDto(BaseModel):
+        message: str
+        n_similar: int
+
+    class DistanceSimilarItemServDto(BaseModel):
+        distances: list
+        similar_items: list
+
+    class UpdateDuplicateMessageServDto(BaseModel):
+        chat_id: int
+        client_message_id: int
+        room_id: int
+        user_id: int
+        message: str
+        sent_at: int
+        subject_id: int
