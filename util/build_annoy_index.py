7d91b97161e7f87968f5f320017365db3037a7ff
callendev20 <callendev20@gmail.com>
2024-09-11 10:48:14 +0900

    ♻️ Make build_annoy_index function

--- util/build_annoy_index.py
+++ util/build_annoy_index.py
@@ -1,30 +1,28 @@
+import pandas as pd
 from annoy import AnnoyIndex
 from sklearn.feature_extraction.text import TfidfVectorizer
-import pandas as pd
+import pickle
 from util.database import engine
 
+def build_annoy_index():
+    with engine.connect() as connection:
+        df = pd.read_sql_table('TbKaMessage', con=connection)
 
+    # TF-IDF 벡터화
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(df['message']).toarray()
 
+    # Annoy 인덱스 생성
+    f = tfidf_matrix.shape[1]  # 벡터의 차원
+    annoy_index = AnnoyIndex(f, 'angular')
 
-with engine.connect() as connection:
-    df = pd.read_sql_table('TbKaMessage', con=connection)
+    # 각 벡터를 Annoy 인덱스에 추가
+    for i, vector in enumerate(tfidf_matrix):
+        annoy_index.add_item(i, vector)
 
-# TF-IDF 벡터화
-tfidf_vectorizer = TfidfVectorizer()
-tfidf_matrix = tfidf_vectorizer.fit_transform(df['message']).toarray()
+    annoy_index.build(n_trees=10)
+    annoy_index.save('../artifacts/annoy_index.ann')
 
-# Annoy 인덱스 생성
-f = tfidf_matrix.shape[1]  # 벡터의 차원
-annoy_index = AnnoyIndex(f, 'angular')
-
-# 각 벡터를 Annoy 인덱스에 추가
-for i, vector in enumerate(tfidf_matrix):
-    annoy_index.add_item(i, vector)
-
-annoy_index.build(n_trees=10)
-annoy_index.save('../artifacts/annoy_index.ann')
-
-# TF-IDF 벡터화 모델 저장
-import pickle
-with open('../artifacts/tfidf_vectorizer.pkl', 'wb') as f:
-    pickle.dump(tfidf_vectorizer, f)
+    # TF-IDF 벡터화 모델 저장
+    with open('../artifacts/tfidf_vectorizer.pkl', 'wb') as f:
+        pickle.dump(tfidf_vectorizer, f)
