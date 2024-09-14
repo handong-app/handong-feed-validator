7c2be321235cb7fe0aef075d326d32eb3acb2489
callendev20 <callendev20@gmail.com>
2024-09-14 22:49:06 +0900

    :card_file_box: Modify field, delete id / add distance, threshold, similar_chat_id

--- models/tb_ka_message.py
+++ models/tb_ka_message.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, String, DateTime, BigInteger, Integer, Text
-import uuid
+from sqlalchemy import Column, String, DateTime, BigInteger, Integer, Text, Float
 
 from util.date_tool import get_seoul_time
 from util.database import Base
@@ -8,12 +7,14 @@ from util.database import Base
 class TbKaMessage(Base):
     __tablename__ = "TbKaMessage"
 
-    id = Column(String(32), primary_key=True, default=lambda: str(uuid.uuid4()).replace('-', ''))
-    chat_id = Column(BigInteger, nullable=False)
+    chat_id = Column(BigInteger, primary_key=True, nullable=False)
     client_message_id = Column(BigInteger, nullable=False)
     room_id = Column(BigInteger, nullable=False)
     user_id = Column(BigInteger, nullable=False)
     message = Column(Text, nullable=False)
+    distance = Column(Float, nullable=False)
+    threshold = Column(Float, nullable=False)
+    similar_chat_id = Column(BigInteger, nullable=False)
     created_at = Column(DateTime, default=get_seoul_time)
     updated_at = Column(DateTime, default=get_seoul_time, onupdate=get_seoul_time)
     last_sent_at = Column(Integer)
