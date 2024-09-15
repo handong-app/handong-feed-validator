068007c965a46f831a123ba8dabd52714b630f6e
callendev20 <callendev20@gmail.com>
2024-09-15 22:22:16 +0900

    :card_file_box: Modify field of TbKaMessage - Add id, Make similar_id not null

--- models/tb_ka_message.py
+++ models/tb_ka_message.py
@@ -1,3 +1,4 @@
+import uuid
 from sqlalchemy import Column, String, DateTime, BigInteger, Integer, Text, Float
 
 from util.date_tool import get_seoul_time
@@ -7,14 +8,15 @@ from util.database import Base
 class TbKaMessage(Base):
     __tablename__ = "TbKaMessage"
 
-    chat_id = Column(BigInteger, primary_key=True, nullable=False)
+    id = Column(String(32), primary_key=True, default=lambda: str(uuid.uuid4()).replace('-', ''))
+    chat_id = Column(BigInteger, nullable=False)
     client_message_id = Column(BigInteger, nullable=False)
     room_id = Column(BigInteger, nullable=False)
     user_id = Column(BigInteger, nullable=False)
     message = Column(Text, nullable=False)
     distance = Column(Float, nullable=False)
     threshold = Column(Float, nullable=False)
-    similar_chat_id = Column(BigInteger, nullable=False)
+    similar_id = Column(String(32), nullable=False)
     created_at = Column(DateTime, default=get_seoul_time)
     updated_at = Column(DateTime, default=get_seoul_time, onupdate=get_seoul_time)
     last_sent_at = Column(Integer)
