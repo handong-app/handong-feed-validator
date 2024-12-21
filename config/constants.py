3b60a43eb1de37f5763a3105f780e56c5bf8b7d2
callendev20 <callendev20@gmail.com>
2024-12-21 17:33:12 +0900

    🔧 Add constants.py

---
+++ config/constants.py
@@ -0,0 +1,4 @@
+ANNOY_INDEX_ALL = './artifacts/annoy_index_all.ann'
+TFIDF_VECTORIZER_ALL = './artifacts/tfidf_vectorizer_all.pkl'
+ANNOY_INDEX_LAST_14DAYS = './artifacts/annoy_index_last_14days.ann'
+TFIDF_VECTORIZER_LAST_14DAYS = './artifacts/tfidf_vectorizer_last_14days.pkl'
