cce08aaeff7026d6cdb6cb8138bb978537a0bdff
callendev20 <callendev20@gmail.com>
2024-12-21 17:48:58 +0900

    🔧 Modify constants.py structure

--- config/constants.py
+++ config/constants.py
@@ -1,4 +1,9 @@
-ANNOY_INDEX_ALL = './artifacts/annoy_index_all.ann'
-TFIDF_VECTORIZER_ALL = './artifacts/tfidf_vectorizer_all.pkl'
-ANNOY_INDEX_LAST_14DAYS = './artifacts/annoy_index_last_14days.ann'
-TFIDF_VECTORIZER_LAST_14DAYS = './artifacts/tfidf_vectorizer_last_14days.pkl'
+class LocalPath:
+    ARTIFACTS = './artifacts'
+    ANNOY_INDEX_ALL = './artifacts/annoy_index_all.ann'
+    TFIDF_VECTORIZER_ALL = './artifacts/tfidf_vectorizer_all.pkl'
+    ANNOY_INDEX_LAST_14DAYS = './artifacts/annoy_index_last_14days.ann'
+    TFIDF_VECTORIZER_LAST_14DAYS = './artifacts/tfidf_vectorizer_last_14days.pkl'
+
+class DatabaseConfig:
+    GET_TbKaMessage_LAST_14DAYS = "SELECT * FROM TbKaMessage WHERE created_at >= NOW() - INTERVAL 14 DAY"
