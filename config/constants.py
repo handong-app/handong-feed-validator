559f8d268799b90cfa84772bd8963eca3659bc88
callendev20 <callendev20@gmail.com>
2024-12-22 14:29:24 +0900

    ✅Add ORDER_BY to ensure order is always guaranteed - DatabaseConfig

--- config/constants.py
+++ config/constants.py
@@ -4,6 +4,12 @@ class LocalPath:
     TFIDF_VECTORIZER_ALL = './artifacts/tfidf_vectorizer_all.pkl'
     ANNOY_INDEX_LAST_14DAYS = './artifacts/annoy_index_last_14days.ann'
     TFIDF_VECTORIZER_LAST_14DAYS = './artifacts/tfidf_vectorizer_last_14days.pkl'
+    LAST_14DAYS_DF = './artifacts/last_14days_df.pkl'
 
 class DatabaseConfig:
-    GET_TbKaMessage_LAST_14DAYS = "SELECT * FROM TbKaMessage WHERE updated_at >= NOW() - INTERVAL 14 DAY"
+    GET_TbKaMessage_LAST_14DAYS = """
+        SELECT * 
+        FROM TbKaMessage 
+        WHERE updated_at >= NOW() - INTERVAL 14 DAY 
+        ORDER BY updated_at ASC, id ASC
+    """
