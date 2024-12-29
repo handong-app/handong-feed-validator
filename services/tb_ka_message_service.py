473e3a191a07ef680c5b9088fd402f5dd9ebe2c2
callendev20 <callendev20@gmail.com>
2024-12-29 18:39:49 +0900

    :fire: Remove script call in TbKaMessageService.new_message_routine

--- services/tb_ka_message_service.py
+++ services/tb_ka_message_service.py
@@ -5,7 +5,6 @@ from config.constants import DatabaseConfig
 from models import TbKaMessage
 from schemas.tb_ka_message_dto import TbKaMessageDto
 from schemas.validate_dto import ValidateDto
-from util.build_annoy_index_last_14days import build_annoy_index_last_14days
 from util.date_tool import get_seoul_time
 
 
@@ -27,9 +26,6 @@ class TbKaMessageService:
             )
             session.add(new_message)
             session.commit()
-
-            # 새 Message 저장 후 index build
-            build_annoy_index_last_14days()
 
             return new_message
         except Exception as e:
